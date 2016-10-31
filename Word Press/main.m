76c6c6a91ec77e3edf38d0e6d56df1853dd89219
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-10-31 19:23:19 -0400

    Working on Word effects

--- Word Press/main.m
+++ Word Press/main.m
@@ -9,9 +9,53 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+    
+    
+    char inputChars[255];
+    
+    printf("input a string");
+    
+    fgets(inputChars, 255, stdin);
+    
+    printf("your string is %s\n", inputChars);
+    
+    NSString *inputString = [NSString stringWithUTF8String:inputChars];
+    
+    NSLog(@"Input was: %@", inputString);
+    
+    
+    NSString *UpperCase = [inputString uppercaseString];
+    
+    NSLog(@"Uppercase input was: %@", UpperCase);
+    
+    NSString *LowerCase = [inputString lowercaseString];
+    
+    NSLog(@"Lowercase input was: %@", LowerCase);
+    
+    NSUInteger CharacterCount = [inputString length];
+    
+    NSLog(@"CharachterCount input was: %lu", (unsigned long)CharacterCount);
+
+    
+    NSString *ehWord = [inputString stringByAppendingString:@" eh"];
+    
+    NSLog(@"ehword input was: %@", ehWord);
+
+    
+    
+    
+    
+    
 }
+
+
+    //NSString *upperCase = [inputString uppercaseStringWithLocale:[NSLocale currentLocale]];
+
+    //NSString *UpperCase = [[inputString objectAtIndex:2] uppercaseString];
+
+    
+    
+
+
+
+
