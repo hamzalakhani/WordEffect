7ae57309f04b77aee82cfebc9de6fc335ae4d0ba
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-10-31 16:56:44 -0400

    Initial Commit

---
+++ Word Press/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Press
+//
+//  Created by Hamza Lakhani on 2016-10-31.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
